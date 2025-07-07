34f26da92c1220ef8aab86255393e8874d6d887b
satyajit1206 <satyajitsahoo1206@gmail.com>
2025-07-07 23:50:43 +0530

    Add Place model with schema definition
    Introduces a new Mongoose model for places, including fields for title, description, location, images, user reference, reviews, and geometry. This schema supports geospatial data and references to related user and review documents.

---
+++ models/places.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ImageSchema = new Schema({
+  url: String,
+  imageId: String
+});
+
+const placeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    images: [ImageSchema],  // Array of images
+    addedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Place", placeSchema);
